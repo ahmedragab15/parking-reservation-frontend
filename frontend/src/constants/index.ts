f2cb072fcf37052302828773e714375f9e2d2824
ahmedragab15 <ragabahmed154@gmail.com>
2025-08-30 08:53:08 +0300

    chore(constants): add nav items for header navigation

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,18 @@
+export const navItems: INavItems[] = [
+    {
+        name: "Home",
+        href: "/",
+    },
+    {
+        name: "Gate Check-in",
+        href: "/gate-checkin",
+    },
+    {
+        name: "Employee Checkout",
+        href: "/employee-checkout",
+    },
+    {
+        name: "Admin Dashboard",
+        href: "/admin-dashboard",
+    },
+];
